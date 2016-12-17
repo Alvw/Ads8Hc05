20a361710e17e561d8d9075309966de30fcf60ea
Alvw <flashvw@gmail.com>
2016-12-17 20:04:44 +0300

    Безусловная перезагрузка BT модуля при отсутсвии PingCommand с компьютера > чем ResetTimout.

--- main.c
+++ main.c
@@ -202,7 +202,7 @@ __interrupt void TimerA_ISR(void)
     P3OUT |= BIT7;//BT reset pin hi
     rfResetCntr = 0;
   }
-  if(isRecording && rfConStat){
+  if(isRecording){
     if(resetTimeout){
       pingCntr++;
       if(pingCntr > resetTimeout){//no signal from host for ~ resetTimeout * 4 seconds
